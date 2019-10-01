5882c33c306f1f233ac05db40cb2d1c8535b565f
lorenzobarilla <37076142+lorenzobarilla@users.noreply.github.com>
2019-10-02 01:14:48 +0200

    mouse fixed

--- sketch.js
+++ sketch.js
@@ -1,63 +1,59 @@
 function preload(){
-  // put preload code here
 }
 
 function setup() {
-  // put setup code here
   createCanvas(windowWidth,windowHeight);
   background('black');
-}
 
-function draw() {
   translate(windowWidth/2,windowHeight/2);
 
-    var index = floor(random() * colorList.length);
-    var index2 = floor(random() * colorList2.length);
-    var index3 = floor(random() * colorList3.length);
-    var index4 = floor(random() * colorList4.length);
-    var index5 = floor(random() * colorList5.length);
+  var index = floor(random() * colorList.length);
+  var index2 = floor(random() * colorList2.length);
+  var index3 = floor(random() * colorList3.length);
+  var index4 = floor(random() * colorList4.length);
+  var index5 = floor(random() * colorList5.length);
 
-    noFill();
-    strokeWeight(0.50);
-    var val1 = windowWidth/20;
+  noFill();
+  strokeWeight(0.50);
+  var val1 = windowWidth/20;
 
-  for (var y = -windowHeight; y < windowHeight; y+=windowWidth/24){
-    for (var x = -windowWidth; x < windowWidth; x+=windowWidth/24){
-      for(var i = val1; i<=5*val1; i+=val1){
+for (var y = -windowHeight; y < windowHeight; y+=windowWidth/24){
+  for (var x = -windowWidth; x < windowWidth; x+=windowWidth/24){
+    for(var i = val1; i<=5*val1; i+=val1){
 
-        if(i == 5*val1){
-          noLoop();
-        }
 
-        if(i == 1*windowWidth/20){
-          stroke(color(colorList[index]));
-        }
-        else if(i == 2*windowWidth/20){
-          stroke(color(colorList2[index2]));
-        }
-        else if(i == 3*windowWidth/20){
-          stroke(color(colorList3[index3]));
-        }
-        else if(i == 4*windowWidth/20){
-          stroke(color(colorList4[index4]));
-        }
-        else if(i == 5*windowWidth/20){
-          stroke(color(colorList5[index5]));
-        }
-
-        ellipse(x, y, i);
-        //console.log(i/val1);
+      if(i == 1*windowWidth/20){
+        stroke(color(colorList[index]));
       }
+      else if(i == 2*windowWidth/20){
+        stroke(color(colorList2[index2]));
+      }
+      else if(i == 3*windowWidth/20){
+        stroke(color(colorList3[index3]));
+      }
+      else if(i == 4*windowWidth/20){
+        stroke(color(colorList4[index4]));
+      }
+      else if(i == 5*windowWidth/20){
+        stroke(color(colorList5[index5]));
+      }
+
+      ellipse(x, y, i);
+
     }
   }
+}
 
-  if (mouseIsPressed) {
-     document.location.reload();
-   }
+}
+function draw() {
+if (mouseIsPressed) {
+   location = location;
+ }
+}
 
-  }
-  var colorList = ['orange', 'blue', 'green', 'pink', 'yellow'];
-  var colorList2 = ['orange', 'blue', 'green', 'pink', 'yellow'];
-  var colorList3 = ['orange', 'blue', 'green', 'pink', 'yellow'];
-  var colorList4 = ['orange', 'blue', 'green', 'pink', 'yellow'];
-  var colorList5 = ['orange', 'blue', 'green', 'pink', 'yellow'];
+
+var colorList = ['orange', 'blue', 'green', 'pink', 'yellow'];
+var colorList2 = ['orange', 'blue', 'green', 'pink', 'yellow'];
+var colorList3 = ['orange', 'blue', 'green', 'pink', 'yellow'];
+var colorList4 = ['orange', 'blue', 'green', 'pink', 'yellow'];
+var colorList5 = ['orange', 'blue', 'green', 'pink', 'yellow'];
